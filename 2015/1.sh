d304e17c4b187f2f47c2c194cdb677c363ffde1a
Paul Kao <paulkao94@gmail.com>
2020-05-30 20:35:26 -0700

    Solution for 1st problem

--- 2015/1.sh
+++ 2015/1.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+FLOOR=0
+COUNT=0
+FILE_PATH=$1
+CONTENT=`cat $FILE_PATH`
+
+while read -n 1 c; do
+  echo "$c"
+  if [ $FLOOR -lt 0 ]; then
+    break
+  fi
+  if [ "$c" = "(" ]; then
+    FLOOR=$((FLOOR+1 ))
+    echo $FLOOR
+  else 
+    FLOOR=$(( FLOOR-1 ))
+  fi
+  COUNT=$(( COUNT+1 ))
+done < "$FILE_PATH"
+echo $FLOOR
+echo $COUNT
